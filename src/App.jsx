7475692fcd4768b199baeaa85868640c55c03e0b
asmedas <sjn1995@hotmail.com>
2025-12-10 09:17:31 +0100

    advertisement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Header from './components/header/Header'
 import MainLayout from './components/mainlayout/MainLayout'
 import './App.css'
@@ -6,10 +5,10 @@ import './App.css'
 function App() {
 
   return (
-    <div className='glass-layout'>
+    <>
       <Header/>
       <MainLayout />
-    </div>
+    </>
 
   )
 }
--- src/components/advertisement/Advertisement.jsx
+++ src/components/advertisement/Advertisement.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Advertisement.css'
+import Audiadd from '../../assets/Audiadd.png';
+import BMWadd from '../../assets/BMWadd.png';
+import Mercedesadd from '../../assets/Mercedesadd.png';
+import Volkswagenadd from '../../assets/Volkswagenadd.png';
 
-function Advertisement (){
+
+export default function Advertisement (){
     return (
         <div className='advertisement'>
-            <h3>reklam av något slag</h3>
+            <a href='https://www.bmw.com/en/index.html' target="_blank" rel="noreferrer">
+                <img src={BMWadd} alt="Audi Advertisement" className='ad-image'/>
+            </a>
+            <a href='https://www.mercedes-benz.com/en/' target="_blank" rel="noreferrer">
+                <img src={Mercedesadd} alt="Audi Advertisement" className='ad-image'/>
+            </a>
+            <a href='https://www.audi.com/en/' target="_blank" rel="noreferrer">
+                <img src={Audiadd} alt="Audi Advertisement" className='ad-image'/>
+            </a>
+            <a href='https://www.volkswagen.se/sv.html' target="_blank" rel="noreferrer">
+                <img src={Volkswagenadd} alt="Audi Advertisement" className='ad-image'/>
+            </a>
         </div>
     )
 }
-
-export default Advertisement
